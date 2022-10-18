5ffe61c0c4dbc2d8411bfafdadf70afe434ced75
Harry Oh <harry@5004.pe.kr>
2022-10-18 21:58:05 +0900

    [OR-319] Change L1 rpc endpoint

--- tokamak-optimism/tokamak-optimism.sh
+++ tokamak-optimism/tokamak-optimism.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -e
 
-ACTION=$1
-ACTION_LIST=("create delete")
-CLUSTER_NAME=$2
-CLUSTER_LIST=$(ls -d kustomize/overlays/*/* | cut -f3-4 -d'/')
-CLUSTER_PATH=kustomize/overlays/${CLUSTER_NAME}
-
 function print_help() {
   echo "Usage: "
-  echo "  $0 [command] [cluster name]"
+  echo "  $0 [command] [cluster name] [env name]"
   echo "    * command list"
-  echo "    - create"
-  echo "    - delete"
+  echo "      - create"
+  echo "      - delete"
+  echo "    * env list"
+  echo "      - aws"
+  echo "      - local"
 }
+
+MYPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ACTION=$1
+ACTION_LIST=("create delete list")
+CLUSTER_NAME=$2
+ENV_NAME=$3
+
+if [[ -z "$ACTION" || -z "$CLUSTER_NAME" || -z "$ENV_NAME" ]]; then
+  print_help
+  exit 1
+fi
+
+ENV_LIST=$(ls -d $MYPATH/kustomize/overlays/*/ | rev | cut -f2 -d'/' |rev)
+CLUSTER_LIST=$(ls -d $MYPATH/kustomize/overlays/${ENV_NAME}/*/ | rev | cut -f2 -d'/' | rev)
+CLUSTER_LIST=${CLUSTER_LIST//templates/}
+CLUSTER_PATH=$MYPATH/kustomize/overlays/${ENV_NAME}/${CLUSTER_NAME}
 
 function check_cluster() {
   for i in $CLUSTER_LIST; do
@@ -28,6 +41,13 @@ function check_action() {
   return 1
 }
 
+function check_env() {
+  for i in $ENV_LIST; do
+    [[ "$i" == "$1" ]] && return 0
+  done
+  return 1
+}
+
 if !(check_action $ACTION); then
   print_help
   exit 1
@@ -37,18 +57,24 @@ if !(check_cluster $CLUSTER_NAME); then
   print_help
   echo -e "\nError: wrong cluster name( $CLUSTER_NAME )"
   echo "Cluster List:"
-  for i in $CLUSTER_LIST; do echo ${i%%/}; done
+  for i in $CLUSTER_LIST; do echo - ${i%%/}; done
   exit 1
 fi
 
-NETWORK=${CLUSTER_NAME##*/}
+if !(check_env $ENV_NAME); then
+  print_help
+  echo -e "\nError: wrong env name($ENV_NAME)"
+  echo "ENV List:"
+  for i in $ENV_LIST; do echo - ${i%%/}; done
+  exit 1
+fi
 
 echo "* ACTION=${ACTION}"
 echo "* CLUSTER_NAME=${CLUSTER_NAME}"
-echo "* NETWORK=${NETWORK}"
+echo "* ENV_NAME=${ENV_NAME}"
 echo
 
-SECRET_FILE=./kustomize/envs/$NETWORK/secret.env
+SECRET_FILE=$MYPATH/kustomize/envs/$CLUSTER_NAME/secret.env
 
 if [ ! -f $SECRET_FILE ]; then
   echo "Not found secret.env file($SECRET_FILE)"
@@ -59,6 +85,7 @@ fi
 if [ $ACTION == "create" ]; then
   if [ -f $CLUSTER_PATH/create.sh ]; then
     sh $CLUSTER_PATH/create.sh
+    [ $? -ne 0 ] && exit 1
   fi
   kubectl apply -k $CLUSTER_PATH
 
@@ -66,6 +93,7 @@ elif [ $ACTION == "delete" ]; then
   kubectl delete -k $CLUSTER_PATH
   if [ -f $CLUSTER_PATH/delete.sh ]; then
     sh $CLUSTER_PATH/delete.sh
+    [ $? -ne 0 ] && exit 1
   fi
 else
   print_help

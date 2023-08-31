b24250bf1c1f4a34f7e87f7d2dffba082bda28a1
rlgns98kr <rlgns98kr@gmail.com>
2023-08-31 17:34:01 +0000

    [OR-953] add policy for fargate log and fix secretmanager bug

--- terraform/modules/aws/eks/main.tf
+++ terraform/modules/aws/eks/main.tf
@@ -21,3 +21,31 @@ module "eks" {
     }
   }
 }
+
+data "aws_iam_policy_document" "aws_fargate_logging_policy" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "aws_fargate_logging_policy" {
+  name   = "aws_fargate_logging_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.aws_fargate_logging_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "aws_fargate_logging_policy_attach_role" {
+  role       = module.eks.fargate_profiles["default"].iam_role_name
+  policy_arn = aws_iam_policy.aws_fargate_logging_policy.arn
+}

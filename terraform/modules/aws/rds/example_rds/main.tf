1ac72268c15ae1724c8ba68127fb6269a52ceba2
ohbyeongmin <fbottlem@gmail.com>
2024-01-05 07:45:10 +0000

    [OR-1254] create RDS terraform

---
+++ terraform/modules/aws/rds/example_rds/main.tf
@@ -0,0 +1,23 @@
+module "example_rds" {
+  source = "../_module/postgres"
+
+  vpc_id             = var.vpc_id
+  private_subnet_ids = var.private_subnet_ids
+
+  rds_name          = var.rds_name
+  rds_database_name = var.rds_database_name
+
+
+  rds_allocated_storage = 10
+
+  from_port = 5432
+  to_port   = 5432
+
+  db_parameters = [
+    {
+      name         = "log_connections"
+      value        = "1"
+      apply_method = "pending-reboot"
+    }
+  ]
+}

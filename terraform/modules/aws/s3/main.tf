69444d7a8499e9aa9dfbd46a49c120863a39819c
ohbyeongmin <fbottlem@gmail.com>
2024-01-18 07:52:14 +0000

    [OR-1307] send access log to bucket

--- terraform/modules/aws/s3/main.tf
+++ terraform/modules/aws/s3/main.tf
@@ -1,3 +1,23 @@
 resource "aws_s3_bucket" "s3" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_object" {
+  bucket = aws_s3_bucket.s3.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::600734575887:root"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/op-geth/AWSLogs/156512274928/*"
+    ]
+  }
+}

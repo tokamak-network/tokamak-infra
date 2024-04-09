74d0766707cd9abc80f6099c2d0b824e0dafcfe2
praveen263112 <praveen2714@gmail.com>
2024-04-09 12:22:31 +0530

    [OR-1255] update es installation script and added health check

--- terraform/modules/aws/ec2/elasticsearch/install_elasticsearch.sh
+++ terraform/modules/aws/ec2/elasticsearch/install_elasticsearch.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 
-#log file for auditability and troubleshooting
+# Log file for auditability and troubleshooting
 LOG_FILE="/var/log/elasticsearch_docker_setup.log"
 exec &> >(tee -a "$LOG_FILE")
 
@@ -9,10 +10,6 @@ echo "Starting setup at $(date)"
 # Update the system securely
 echo "Updating the system..."
 sudo yum update -y
-
-# Remove any potentially conflicting packages
-echo "Removing conflicting packages..."
-sudo yum remove -y python3-requests
 
 # Install Docker
 echo "Installing Docker..."
@@ -23,27 +20,22 @@ echo "Enabling and starting Docker..."
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Install pip3 for Docker Compose installation
-echo "Installing pip3..."
-sudo yum install -y python3-pip
-
-# Install a specific version of Docker Python package
-echo "Installing Docker Python package..."
-sudo pip3 install docker==6.1.3
-
 # Install Docker Compose
 echo "Installing Docker Compose..."
-sudo pip3 install docker-compose
+DOCKER_COMPOSE_VERSION="v2.3.3"  
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64" -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 # Verify Docker and Docker Compose installation
 echo "Verifying Docker and Docker Compose installation..."
 sudo docker --version
-sudo docker-compose --version
+sudo docker compose version 
 
 # Assuming docker-compose.yml is already transferred to /tmp
 cd /tmp || exit
 
 echo "Starting Elasticsearch with Docker Compose..."
-sudo docker-compose up -d
+sudo docker compose up -d  # Using the integrated `docker compose` command
 
 echo "Setup completed at $(date)"
